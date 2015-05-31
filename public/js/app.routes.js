8ba749ccad6a2bc636b3618295ffbb96e793ba1b
kylemorton5770 <mikemorton91@gmail.com>
2015-05-31 19:04:43 -0400

    completed routing and template injection

--- public/js/app.routes.js
+++ public/js/app.routes.js
@@ -8,20 +8,20 @@ angular.module('routerRoutes', ['ngRoute'])
 
 		// route for the home page
 		.when('/', {
-			templateUrl : 'views/pages/home.html',
+			templateUrl : 'views/home.html',
 			controller : 'homeController',
 			controllerAs: 'home'
 		})
 
 		// route for the about page
 		.when('/about', {
-			templateUrl : 'views/pages/about.html',
+			templateUrl : 'views/about.html',
 			controller : 'aboutController',
 			controllerAs: 'about'
 		})
 		// route for the contact page
 		.when('/contact', {
-			templateUrl : 'views/pages/contact.html',
+			templateUrl : 'views/contact.html',
 			controller : 'contactController',
 			controllerAs: 'contact'
 		});
